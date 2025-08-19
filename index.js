cfd82640b3927c2584012eee2107bb46d700aca5
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-08-20 00:40:55 +0300

    feat: add page for choosing the character

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 "use strict";
 
 const body = document.querySelector("body");
+let winsCount = 0;
+let losesCount = 0;
+let usingAvatar = 1;
+
+const characters = [
+  "./assets/img/character1.png",
+  "./assets/img/character2.png",
+  "./assets/img/character3.png",
+  "./assets/img/character4.png",
+  "./assets/img/character5.png",
+  "./assets/img/character6.png",
+];
 
 if (!localStorage.getItem("character")) {
-  initialPage();
+  createInitialPage();
 } else {
-  // let gamer = JSON.parse(localStorage.getItem("character"));
-  // console.log(gamer);
   createHomePage();
 }
 
@@ -16,7 +26,7 @@ function createElement(tag, nameForClass, elemForInsert) {
   elemForInsert.append(elem);
 }
 
-function initialPage() {
+function createInitialPage() {
   createElement("div", "registration-field", body);
 
   const registrationField = document.querySelector(".registration-field");
@@ -59,33 +69,141 @@ function createHomePage() {
   createElement("button", "button_fight", body);
   const button = document.querySelector(".button_fight");
   button.innerText = "Fight!";
+
+  createElement("button", "button_character", body);
+  const character = document.querySelector(".button_character");
+  character.innerText = "Character";
+
+  character.addEventListener("click", () => {
+    let gamer = JSON.parse(localStorage.getItem("character"));
+    console.log("hi");
+    body.innerHTML = "";
+    createCharacterPage();
+    const avatar = document.querySelector(".button_avatar");
+
+    if (avatar) {
+      console.log("Hi again");
+      avatar.addEventListener("click", () => {
+        createElement("div", "background", body);
+        const background = document.querySelector(".background");
+        createElement("div", "select-field", background);
+        const select = document.querySelector(".select-field");
+
+        createElement("div", "cross", select);
+        const cross = document.querySelector(".cross");
+        createElement("span", "cross-span", cross);
+        createElement("span", "cross-span", cross);
+
+        for (let i = 0; i < characters.length; i++) {
+          const label = document.createElement("label");
+          select.append(label);
+          const input = document.createElement("input");
+          input.classList.add("input-for-img");
+          input.setAttribute("type", "radio");
+          input.setAttribute("name", "image");
+          input.setAttribute("value", `${characters[i]}`);
+          label.append(input);
+
+          const wrapper = document.createElement("div");
+          wrapper.classList.add("wrapper-for-img");
+          label.append(wrapper);
+
+          const img = document.createElement("img");
+          img.classList.add("choose-character-img");
+          img.setAttribute("alt", `Character image ${i + 1}`);
+          img.setAttribute("width", "180px");
+          img.setAttribute("height", "240px");
+          img.setAttribute("src", `${characters[i]}`);
+          wrapper.append(img);
+          img.addEventListener("click", () => {
+            gamer[0].usingAvatar = +img.alt.at(-1);
+            localStorage.setItem("character", JSON.stringify(gamer));
+          });
+
+          if (gamer[0].usingAvatar === i + 1) {
+            input.setAttribute("checked", "");
+          }
+
+          cross.addEventListener("click", () => {
+            background.remove();
+          });
+        }
+
+        const images = document.getElementsByName("image");
+        // const selectedImage = document.querySelector(".selected-image");
+
+        images.forEach((image) => {
+          image.addEventListener("change", function () {
+            const selectedImage = document.querySelector(".character-img");
+            selectedImage.src = this.value;
+          });
+        });
+      });
+    }
+  });
+}
+
+function createCharacterPage() {
+  let gamer = JSON.parse(localStorage.getItem("character"));
+  createElement("h1", "character-name", body);
+  const name = document.querySelector(".character-name");
+  name.innerText = `Name: ${gamer[0].name}`;
+
+  createElement("div", "character-page", body);
+  const page = document.querySelector(".character-page");
+
+  createElement("img", "character-img", page);
+  const img = document.querySelector(".character-img");
+  img.setAttribute("alt", "Character image 1");
+  img.setAttribute("width", "480px");
+  img.setAttribute("height", "540px");
+  img.setAttribute("src", `${characters[gamer[0].usingAvatar - 1]}`);
+
+  createElement("div", "info-character", page);
+  const info = document.querySelector(".info-character");
+
+  createElement("p", "count-of-wins", info);
+  const wins = document.querySelector(".count-of-wins");
+  wins.innerText = `Wins: ${winsCount}`;
+
+  createElement("p", "count-of-loses", info);
+  const loses = document.querySelector(".count-of-loses");
+  loses.innerText = `Loses: ${losesCount}`;
+
+  createElement("button", "button_avatar", info);
+  const avatar = document.querySelector(".button_avatar");
+  avatar.innerText = "Choose avatar";
 }
 
 const button = document.querySelector(".registration-field__button");
-button.addEventListener("click", () => {
-  const input = document.querySelector(".registration-field__input-class");
 
-  if (input.value.length === 0) {
-    const tooltip = document.querySelector(".tooltip");
-    tooltip.style.opacity = "1";
-    tooltip.style.zIndex = "1";
+if (button) {
+  button.addEventListener("click", () => {
+    const input = document.querySelector(".registration-field__input-class");
 
-    setTimeout(() => {
-      tooltip.style.opacity = "0";
-      tooltip.style.zIndex = "-1";
-      input.focus();
-    }, 3000);
-  } else {
-    localStorage.setItem(
-      "character",
-      JSON.stringify([
-        {
-          name: input.value,
-        },
-      ])
-    );
+    if (input.value.length === 0) {
+      const tooltip = document.querySelector(".tooltip");
+      tooltip.style.opacity = "1";
+      tooltip.style.zIndex = "1";
 
-    body.innerHTML = "";
-    createHomePage();
-  }
-});
+      setTimeout(() => {
+        tooltip.style.opacity = "0";
+        tooltip.style.zIndex = "-1";
+        input.focus();
+      }, 3000);
+    } else {
+      localStorage.setItem(
+        "character",
+        JSON.stringify([
+          {
+            name: input.value,
+            usingAvatar: 1,
+          },
+        ])
+      );
+
+      body.innerHTML = "";
+      createHomePage();
+    }
+  });
+}
